13d05f791cad7117189633e6ae83f0d644da56a7
jonathan_fambro <startek591@gmail.com>
2024-11-14 23:23:00 -0500

    Reviewing Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -74,3 +74,24 @@ function fruitProcessor(apples, oranges) {
   return juice;
 }
 console.log(fruitProcessor(2, 3));
+
+// Reviewing Functions
+const calcAge4 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearsUntilRetirement2 = function (birthYear, firstName) {
+  const age = calcAge4(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired 🎉`);
+    return -1;
+  }
+};
+
+console.log(yearsUntilRetirement2(1991, 'Jonas'));
+console.log(yearsUntilRetirement2(1950, 'Mike'));
