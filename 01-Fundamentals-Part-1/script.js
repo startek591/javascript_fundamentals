2826f593bf7648838b93ed4cfd9525497d5904ba
jonathan_fambro <startek591@gmail.com>
2024-11-14 21:32:21 -0500

    The Switch Statement

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -229,4 +229,29 @@
   } else {
     console.log('Someone else should drive...');
   }
+
+  // The switch Statement
+  const day = 'friday';
+  switch (day) {
+    case 'monday':
+      console.log('Plan course structure');
+      console.log('Go to coding meetup');
+      break;
+    case 'tuesday':
+      console.log('Prepare theory videos');
+      break;
+    case 'wednesday':
+    case 'thursday':
+      console.log('Write code examples');
+      break;
+    case 'friday':
+      console.log('Record vidoes');
+      break;
+    case 'saturday':
+    case 'sunday':
+      console.log('Enjoy the weekend :D');
+      break;
+    default:
+      console.log('Not a valid day!');
+  }
 })();
