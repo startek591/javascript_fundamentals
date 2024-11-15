44967bacc48abb0588826d5c9bee86bf5a354343
jonathan_fambro <startek591@gmail.com>
2024-11-15 01:02:54 -0500

    Looping Backwards and Loops in Loops

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -302,3 +302,27 @@ for (let i = 0; i < jonas5.length; i++) {
   if (typeof jonas5[i] === 'number') break;
   console.log(jonas5[i], typeof jonas5[i]);
 }
+
+// Looping Backwards and Loops in Loops
+const jonas6 = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true,
+];
+
+for (let i = jonas6.length - 1; i >= 0; i--) {
+  console.log(i, jonas6[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`------- Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(
+      `Exercise ${exercise}: Lifting weight repetition ${rep} 🏋🏿‍♂️`
+    );
+  }
+}
