6972e705f1fec48879e90f8ebfa2213781c8102b
jonathan_fambro <startek591@gmail.com>
2024-11-14 21:21:22 -0500

    Equality Operators: == vs. ===

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -191,4 +191,28 @@
   } else {
     console.log('Height is UNDEFINED');
   }
+
+  // Equality Operators: == vs. ===
+  const age3 = '18';
+  if (age3 === 18)
+    console.log('You just became an adult :D (strict)');
+
+  if (age3 == 18) console.log('You just became an adult :D (loose)');
+
+  const favourite = Number(prompt("What's your favourite number?"));
+  console.log(favourite);
+  console.log(typeof favourite);
+
+  if (favourite === 23) {
+    // 22 === 23 -> FALSE
+    console.log('Cool! 23 is an amzaing number!');
+  } else if (favourite === 7) {
+    console.log('7 is also a cool number');
+  } else if (favourite === 9) {
+    console.log('9 is also a cool number');
+  } else {
+    console.log('Number is not 23 or 7 or 9');
+  }
+
+  if (favourite !== 23) console.log('Why not 23?');
 })();
