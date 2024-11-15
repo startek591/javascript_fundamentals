1fec223fb5696f612ace821e3bc1785a5c7f136e
jonathan_fambro <startek591@gmail.com>
2024-11-14 22:51:15 -0500

    Created Fundaments Part 2 Application and Added strict mode

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,9 @@
+// Activating Strict Mode
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+const interface = 'Audio';
+const private = 534;
