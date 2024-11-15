9ed3cb944b0280ba7cc34dea8fe478068412056a
jonathan_fambro <startek591@gmail.com>
2024-11-15 01:04:27 -0500

    While Loop

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -326,3 +326,21 @@ for (let exercise = 1; exercise < 4; exercise++) {
     );
   }
 }
+
+// The while Loop
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+}
+
+let rep = 1;
+while (rep <= 10) {
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log('Loop is about to end...');
+}
