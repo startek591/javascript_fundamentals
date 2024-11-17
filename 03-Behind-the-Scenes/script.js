9f3f6510d0a85bfd589e9eed81c201601d889023
jonathan_fambro <startek591@gmail.com>
2024-11-16 21:58:56 -0500

    Primitives vs Objects in Practice

--- 03-Behind-the-Scenes/script.js
+++ 03-Behind-the-Scenes/script.js
@@ -163,3 +163,38 @@ const friend = me2;
 friend.age = 27;
 console.log('Friend:', friend);
 console.log('Me', me2);
+
+// Primitives vs. Objects in Practice
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// Reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage: ', jessica);
+console.log('After marriage: ', marriedJessica);
+
+// Copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage: ', jessica2);
+console.log('After marriage: ', jessicaCopy);
