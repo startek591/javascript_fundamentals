fb550a6922855b8d8cdc137e0c3f13d2610ecb5b
jonathan_fambro <startek591@gmail.com>
2024-11-17 17:21:59 -0500

    Optional chaining

--- 04-Data-Structures-Operators/script.js
+++ 04-Data-Structures-Operators/script.js
@@ -10,7 +10,7 @@ const openingHours = {
   },
 };
 
-const restuarant = {
+const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
@@ -304,3 +304,30 @@ console.log(entries);
 for (const [day, { open, close }] of entries) {
   console.log(`On ${day} we open at ${open} and close at ${close}`);
 }
+
+// Optional Chaining
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
+}
+
+// WITH optional chaining
+console.log(restaurant.openingHours.thu?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+for (const day of weekdays) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(
+  restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist'
+);
+
+// Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+console.log(users[0]?.name ?? 'User array empty');
+
+if (users.length > 0) console.log(users[0].name);
+else console.log('user array emtpy');
