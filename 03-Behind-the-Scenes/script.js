7e29cd653bc27244a1fb48d9ee6c9cf18ec97e4d
jonathan_fambro <startek591@gmail.com>
2024-11-16 21:21:03 -0500

    Regular Functions vs Arrow Functions

--- 03-Behind-the-Scenes/script.js
+++ 03-Behind-the-Scenes/script.js
@@ -104,3 +104,46 @@ matilda.calcAge();
 
 const f = jonas.calcAge;
 f();
+
+// Regular Functions vs. Arrow Functions
+var firstName2 = 'Matilda';
+
+const jonas2 = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+
+    // Solution 1
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    // Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName2}`);
+  },
+};
+jonas2.greet();
+jonas2.calcAge();
+
+// arguments keyword
+const addExpr2 = function (a, b) {
+  return a + b;
+};
+addExpr2(2, 5);
+addExpr2(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  return a + b;
+};
+addArrow(2, 5, 8);
