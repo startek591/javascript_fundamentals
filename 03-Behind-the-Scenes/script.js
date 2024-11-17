2323a84ba1fbbdadf1eabd3770d9851a47f12973
jonathan_fambro <startek591@gmail.com>
2024-11-16 20:49:35 -0500

    Hoisting and TDZ in Practice

--- 03-Behind-the-Scenes/script.js
+++ 03-Behind-the-Scenes/script.js
@@ -32,3 +32,41 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
+
+// Hoisting and TDZ in Practice
+console.log(me);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDec1(2, 3));
+
+function addDec1(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
